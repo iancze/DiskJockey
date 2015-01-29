d329de2a6356a170b902ce2d7e7233da3653b758
Ian Czekala <iancze@gmail.com>
2015-01-29 18:15:27 -0500

    Trying for machinefile

--- hostgen.sh
+++ hostgen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 hostlist=$(scontrol show hostname $SLURM_JOB_NODELIST)
-rm -f hosts
+rm -f hosts.txt
 
 for f in $hostlist
 do
-  echo $f >> hosts
+  echo $f >> hosts.txt
 done
+
+echo $SLURM_JOB_CPUS_PER_NODE >> hosts.txt
+echo $SLURM_TASKS_PER_NODE >> hosts.txt
---
+++ run_AKSco_dist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##SBATCH -J AKSco #Single job name for the entire JobArray
+
+#SBATCH -o slurm/multi_%A_%a.out #standard output
+
+#SBATCH -e slurm/multi_%A_%a.err #standard error
+
+#SBATCH -p general #partition
+
+#SBATCH -t 00:10:00 #running time
+
+#SBATCH --mail-type=BEGIN
+
+#SBATCH --mail-type=END
+
+#SBATCH --mail-user=iancze@gmail.com
+
+#SBATCH --mem-per-cpu 10 #memory request per node
+
+#SBATCH -n 10
+
+./hostgen.sh
+
+julia --machinefile hosts.txt tests/parallel_test.jl
