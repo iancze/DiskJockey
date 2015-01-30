41bfb56cac1ddb2af7aa31928bd2220786bd954f
Ian Czekala <iancze@gmail.com>
2015-01-30 13:58:01 -0500

    Trying again w/ fewer workers

--- burma_shave.jl
+++ burma_shave.jl
@@ -73,11 +73,11 @@ end
 
 # Since we are starting with a machinefile, it is no longer necessary to
 # programatically add processes.
-# nchild = length(keylist)
+nchild = length(keylist)
 # addprocs(nchild)
-nchild = nworkers()
-println("We have $nchild worker processes.")
-println("They are ", workers())
+# nchild = nworkers()
+# println("We have $nchild worker processes.")
+println("Workers allocated ", workers())
 
 # make the values of run_index and config available on all processes
 for process in procs()
