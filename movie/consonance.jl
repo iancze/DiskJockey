1328f893481ac35df44d7c98aae12bc37089ae53
Ian Czekala <iancze@gmail.com>
2015-02-05 18:01:38 -0500

    Moving to cluster

--- movie/consonance.jl
+++ movie/consonance.jl
@@ -17,17 +17,17 @@ using model
 # outdir = "/n/home07/iczekala/JudithExcalibur/output/movie/" #Where the images.out and images.png are stored
 
 # For local use
-homedir = "/home/ian/Grad/Research/Disks/JudithExcalibur/"
-scratchdir = "/home/ian/Grad/Research/Disks/JudithExcalibur/movie/"
-outdir = "/home/ian/Grad/Research/Disks/JudithExcalibur/movie/"
+# homedir = "/home/ian/Grad/Research/Disks/JudithExcalibur/"
+# scratchdir = "/home/ian/Grad/Research/Disks/JudithExcalibur/movie/"
+# outdir = "/home/ian/Grad/Research/Disks/JudithExcalibur/movie/"
 
 # For cfa0 use
-# homedir = "/pool/scout0/JudithExcalibur/"
-# scratchdir = "/pool/cf/iczekala/scratch/"
-# outdir = "/pool/scout0/JudithExcalibur/output/movie/"
+homedir = "/pool/scout0/JudithExcalibur/"
+scratchdir = "/pool/cf/iczekala/scratch/"
+outdir = "/pool/scout0/JudithExcalibur/output/movie/"
 
 # How many frames per process?
-nframes_per_proc = 4
+nframes_per_proc = 50
 
 global const nchan = 7
 global const vels = linspace(-1.5, 1.5, nchan) # [km/s]
--- movie/find_vmax_gas.jl
+++ movie/find_vmax_gas.jl
@@ -7,22 +7,29 @@ push!(LOAD_PATH, "/pool/scout0/JudithExcalibur/")
 
 using image
 using consonance
+using visibilities
 
 # Store a global max value, and then go through each image, see if it beats
 # this value, if so, record the new one, then close image to keep the memory
 # low.
 
-fname = outdir * @sprintf("dimage%04d.out", 1)
+fname = outdir * @sprintf("gimage%04d.out", 1)
 im = imread(fname)
 skim = imToSky(im, 73.)
+nchan = length(skim.lams)
+println("$nchan channels")
 
+global vmax_g = maximum(skim.data)
+global vmin_g = minimum(skim.data)
 
-global vmax_d = maximum(skim.data)
-global vmin_d = minimum(skim.data)
+vis_fft = transform(skim, 1)
+
+global vmax_v = maximum(abs(vis_fft.VV))
+global vmin_v = minimum(abs(vis_fft.VV))
 
 # Load all of the images into memory.
 # Determine which image*.out files are in this directory.
-imgfunc = x -> contains(x, "dimage") && contains(x, ".out")
+imgfunc = x -> contains(x, "gimage") && contains(x, ".out")
 imglist = filter(imgfunc, readdir(outdir))
 
 nimg = length(imglist)
@@ -33,12 +40,25 @@ for fname in imglist
     im = imread(outdir * fname)
     skim = imToSky(im, 73.)
     min,max = extrema(skim.data)
-    if max > vmax_d
-        vmax_d = max
+    if max > vmax_g
+        vmax_g = max
     end
-    if min < vmin_d
-        vmin_d = min
+    if min < vmin_g
+        vmin_g = min
     end
+    for i=1:nchan
+        vis_fft = transform(skim, i)
+        min,max = extrema(abs(vis_fft.VV))
+        if max > vmax_v
+            vmax_v = max
+        end
+        if min < vmin_v
+            vmin_v = min
+        end
+    end
+
+
 end
 
-println("Dust minimum and maximum $vmin_d, $vmax_d")
+println("Gas minimum and maximum $vmin_g, $vmax_g")
+println("Visibility minimum and maximum $vmin_v, $vmax_v")
