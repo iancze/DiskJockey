ea47e723962607a9ab44afb7eed6b71921ae585b
Ian Czekala <iancze@gmail.com>
2015-02-05 13:38:46 -0500

    changed path

--- movie/make_dmovie.jl
+++ movie/make_dmovie.jl
@@ -3,7 +3,7 @@
 # following https://trac.ffmpeg.org/wiki/Create%20a%20video%20slideshow%20from%20images
 using consonance
 
-ins = outdir * "%04d.png"
+ins = outdir * "d%04d.png"
 outs = outdir * "dust_out.mp4"
 run(`ffmpeg -framerate 10.0 -i $ins -c:v libx264 -r 30 -pix_fmt yuv420p $outs`)
 # run(`ffmpeg -framerate 3.0 -i %04d.png -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4`)
