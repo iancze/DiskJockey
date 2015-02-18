0f73aac53246d6dc2809dad89599827ad9bb9329
Ian Czekala <iancze@gmail.com>
2015-02-18 10:51:10 -0500

    Longer run on Mars

--- scripts/install_packages.jl
+++ scripts/install_packages.jl
@@ -1,6 +1,6 @@
 # Go through and add all required packages
 
-pkgs = ["YAML", "PyPlot", "LaTeXStrings", "Distributions", "PDMats", "HDF5", "ArgParse", "NPZ"]
+pkgs = ["YAML", "PyPlot", "LaTeXStrings", "Distributions", "PDMats", "HDF5", "ArgParse", "NPZ", "Logging"]
 
 for pkg in pkgs
     Pkg.add(pkg)
