bdd1f06e652f9f0f5c7d545a51c123c70b9ab600
Ian Czekala <iancze@gmail.com>
2015-07-14 08:43:23 -0400

    Considating requirements

--- scripts/install_packages.jl
+++
@@ -1,7 +0,0 @@
-# Go through and add all required packages
-
-pkgs = ["YAML", "PyPlot", "LaTeXStrings", "Distributions", "PDMats", "HDF5", "ArgParse", "NPZ", "Logging"]
-
-for pkg in pkgs
-    Pkg.add(pkg)
-end
