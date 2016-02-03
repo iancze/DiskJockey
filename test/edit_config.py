bac00c26c1614eb256df78c8f3cd82ec63749c68
Ian Czekala <iancze@gmail.com>
2016-02-03 08:58:44 -0500

    changing conda build.

--- test/edit_config.py
+++ test/edit_config.py
@@ -15,6 +15,9 @@ f = open("config.yaml")
 config = yaml.load(f)
 f.close()
 
+import numpy as np
+nparam = 12
+nwalkers = 2 * nparam
 
 if args.fix_distance
     # Just change the distance
@@ -27,6 +30,32 @@ if args.model == "standard":
     # Also set parameters to a reasonable dictionary
     config["parameters"] = {"M_star": 1.03, "PA": 152.0, "T_10": 91.85, "dpc": 145.0, "gamma": 1.0, "incl": 45.0, "ksi": 0.2, "logM_gas": -3.8, "mu_DEC": 0.0, "mu_RA": 0.0, "q": 0.5, "r_c": 500.0, "vel": 0.0}
 
+    if args.fix_distance:
+        p0 = np.array([np.random.uniform(1.03, 1.05, nwalkers), # mass [M_sun]
+              np.random.uniform(300., 420.0, nwalkers), #r_c [AU]
+              np.random.uniform(80., 90, nwalkers), #T_10 [K]
+              np.random.uniform(0.55, 0.65, nwalkers), # q
+              np.random.uniform(-4.0, -3.8, nwalkers), #log10 M_gas [log10 M_sun]
+              np.random.uniform(0.4, 0.5, nwalkers), #xi [km/s]
+              np.random.uniform(43.0, 47.0, nwalkers), #inc [degrees]
+              np.random.uniform(152.0, 156.0, nwalkers), #PA [degrees]
+              np.random.uniform(-2.0, -1.9, nwalkers), #vz [km/s]
+              np.random.uniform(0.2, 0.3, nwalkers), #mu_a [arcsec]
+              np.random.uniform(-0.3, -0.1, nwalkers)]) #mu_d [arcsec]
+    else:
+        p0 = np.array([np.random.uniform(1.03, 1.05, nwalkers), # mass [M_sun]
+              np.random.uniform(300., 420.0, nwalkers), #r_c [AU]
+              np.random.uniform(80., 90, nwalkers), #T_10 [K]
+              np.random.uniform(0.55, 0.65, nwalkers), # q
+              np.random.uniform(-4.0, -3.8, nwalkers), #log10 M_gas [log10 M_sun]
+              np.random.uniform(0.4, 0.5, nwalkers), #xi [km/s]
+              np.random.uniform(135.0, 145.0, nwalkers), #dpc [pc]
+              np.random.uniform(43.0, 47.0, nwalkers), #inc [degrees]
+              np.random.uniform(152.0, 156.0, nwalkers), #PA [degrees]
+              np.random.uniform(-2.0, -1.9, nwalkers), #vz [km/s]
+              np.random.uniform(0.2, 0.3, nwalkers), #mu_a [arcsec]
+              np.random.uniform(-0.3, -0.1, nwalkers)]) #mu_d [arcsec]
+
 elif args.model == "truncated":
     config["model"] = "truncated"
     pass
@@ -35,7 +64,38 @@ elif args.model == "cavity":
     config["model"] = "cavity"
     config["parameters"] = {"M_star": 1.03, "PA": 152.0, "T_10": 91.85, "dpc": 145.0, "gamma": 1.0, "incl": 45.0, "ksi": 0.2, "logM_gas": -3.8, "mu_DEC": 0.0, "mu_RA": 0.0, "q": 0.5, "r_c": 500.0, "r_cav":30, "vel": 0.0}
 
+    if args.fix_distance:
+        p0 = np.array([np.random.uniform(1.03, 1.05, nwalkers), # mass [M_sun]
+              np.random.uniform(300., 420.0, nwalkers), #r_c [AU]
+              np.random.uniform(10., 20.0, nwalkers), #r_cav [AU]
+              np.random.uniform(80., 90, nwalkers), #T_10 [K]
+              np.random.uniform(0.55, 0.65, nwalkers), # q
+              np.random.uniform(-4.0, -3.8, nwalkers), #log10 M_gas [log10 M_sun]
+              np.random.uniform(0.4, 0.5, nwalkers), #xi [km/s]
+              np.random.uniform(43.0, 47.0, nwalkers), #inc [degrees]
+              np.random.uniform(152.0, 156.0, nwalkers), #PA [degrees]
+              np.random.uniform(-2.0, -1.9, nwalkers), #vz [km/s]
+              np.random.uniform(0.2, 0.3, nwalkers), #mu_a [arcsec]
+              np.random.uniform(-0.3, -0.1, nwalkers)]) #mu_d [arcsec]
+    else:
+        p0 = np.array([np.random.uniform(1.03, 1.05, nwalkers), # mass [M_sun]
+              np.random.uniform(300., 420.0, nwalkers), #r_c [AU]
+              np.random.uniform(10., 20.0, nwalkers), #r_cav [AU]
+              np.random.uniform(80., 90, nwalkers), #T_10 [K]
+              np.random.uniform(0.55, 0.65, nwalkers), # q
+              np.random.uniform(-4.0, -3.8, nwalkers), #log10 M_gas [log10 M_sun]
+              np.random.uniform(0.4, 0.5, nwalkers), #xi [km/s]
+              np.random.uniform(135.0, 145.0, nwalkers), #dpc [pc]
+              np.random.uniform(43.0, 47.0, nwalkers), #inc [degrees]
+              np.random.uniform(152.0, 156.0, nwalkers), #PA [degrees]
+              np.random.uniform(-2.0, -1.9, nwalkers), #vz [km/s]
+              np.random.uniform(0.2, 0.3, nwalkers), #mu_a [arcsec]
+              np.random.uniform(-0.3, -0.1, nwalkers)]) #mu_d [arcsec]
+
 
 g = open("config.yaml", mode="w")
 yaml.dump(config, g)
 g.close()
+
+# Save the new position file to disk
+np.save("pos0.npy", p0)
