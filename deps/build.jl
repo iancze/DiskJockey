bf1ae762b89ec1651773fb25f5a5be7af8659e69
Ian Czekala <iancze@gmail.com>
2018-10-03 17:39:45 -0700

    Working on v1.0.1

--- deps/build.jl
+++ deps/build.jl
@@ -3,14 +3,12 @@ binary_file = "radmc-3d_v0.41_07.07.17.zip"
 
 # Change to the src directory
 cd("src")
-println("Current working directory ", pwd())
 
 # Download and extract the RADMC-3D executable
 download("http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/$binary_file", binary_file)
-println("Current working directory ", pwd())
 
 # Remove the directory, if a stale version exists from a previous build
-rm("radmc-3d/version_0.41/src", recursive=true)
+rm("radmc-3d", recursive=true)
 
 # Unzipping the file
 run(`unzip $binary_file`)
